8820250f6787a2ee5b72ca18ba8fddb496dbc1a3
SGO Consultora <administracion@sgoconsultora.com>
2022-05-31 22:52:26 -0300

    Se agregan cambios con eventos y DOM modificado para proyecto

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -36,7 +36,7 @@ function validaIngreso(valor, tipo)
                 message="Perfecto!"
                 return message;
             }
-            else
+        else
             {
                 message = "Verifique el dato ingresado!";
                 return message;
@@ -55,16 +55,79 @@ function userValidation(usrName, usrPassword)
     }
 }
 
+function companyValidation() {
+    const inputCompanyName = document.getElementById("inputCompanyName");
+    if (inputCompanyName.value.trim() === "")
+    {
+        const lblCompanyName = document.getElementById("lblCompanyName");
+        lblCompanyName.style.display = "block";
+        lblCompanyName.innerText="El nombre de la empresa no puede estar vacío.";
+        
+    }
+    else
+    {
+       return nombreEmpresa = inputCompanyName.value;
+    }
+}
+
+function baseAmountValidation() {
+    const inputBaseAmount = document.getElementById("inputBaseAmount");
+    if (inputBaseAmount.value.trim() === "" ||  isNaN(inputBaseAmount.value) ||   inputBaseAmount.value<= 0)
+    {
+        const lblBaseAmount = document.getElementById("lblBaseAmount");
+        lblBaseAmount.style.display = "block";
+        lblBaseAmount.innerText="El monto inicial debe ser mayor a 0 (cero)";
+    }
+    else
+    {
+        return mtoImplementacionBaseUsd =inputBaseAmount.value ;
+    }
+}
+
+function usdPriceValidation() {
+    const inputUsDolarPrice = document.getElementById("inputUsDolarPrice");
+    if (inputUsDolarPrice.value.trim() === "" ||  isNaN(inputUsDolarPrice.value) ||   inputUsDolarPrice.value<= 0)
+    {
+        const lblUsDolarPrice = document.getElementById("lblUsDolarPrice");
+        lblUsDolarPrice.style.display = "block";
+        lblUsDolarPrice.innerText="La cotización del dólar debe ser mayor a 0 (cero)";
+    }
+    else
+    {
+        return cotizActualDolar =inputUsDolarPrice.value ;
+    }
+}
+ 
+
 /**************  END VALIDATION FUNCTIONS *******************/
 
-// Welcome
+/*************** BEGIN FRONTEND FUNCTIONS **************** */
 
-alert("Bienvenido al sistema de gestión de cotizaciones. \nLuego de iniciar sesión, seleccione el cliente y servicio a cotizar.")
+function showServices(){
+    const serviceContainer = document.getElementById("services");
+    serviceContainer.style.display='flex';
+}
 
-// Login
+function hideFormSoftware(){
+    const formSoftware = document.getElementById("frmSoftwareParameters");
+    formSoftware.style.display = 'none'
+}
 
-let userName = prompt("Ingrese su usuario:");
-let userPassword = prompt("Ingrese su contraseña:");
+function showFormSoftware(){
+    const formSoftware = document.getElementById("frmSoftwareParameters");
+    formSoftware.style.display='flex';
+    formSoftware.style.flexDirection = 'column';
+}
+
+function hideLabels (){
+    const labels = document.getElementsByClassName("frmSoftwareParameters__lblError");
+    for (let i = 0; i < labels.length; i++) {
+        labels[i].style.display='none';
+              
+    }
+}
+
+/*************** END FRONTEND FUNCTIONS **************** */
 
 class Users
 {
@@ -72,7 +135,6 @@ class Users
    {
        this.usrName = usrName;
        this.usrPassword = usrPassword;
-      
    }
 }
 
@@ -84,146 +146,164 @@ const lstUsers = [
     {usrName: "lvergara", usrPassword: "lvergara123" }
 ]
 
-if (!userValidation(userName, userPassword))
-{
-    alert("ERROR - Verifique en consola los detalles");
-    throw new Error("Su usuario o contraseña no son válidos");
+// Login
+
+const inputUser = document.getElementById("inputUser");
+const  inputPass = document.getElementById("inputPass");
+let btnLogin = document.getElementById("btnLogin");
+
+btnLogin.onclick = () =>{
+    
+    let userName = inputUser.value.trim();
+    let userPassword = inputPass.value;
+    
+   if(userName ==="" ||  userPassword ==="" )
+   {
+       alert("El usuario y la contraseña son obligatorios");
+   }
+   else
+   {
+        if (!userValidation(userName, userPassword))
+        {
+            alert("Su usuario o contraseña no son válidos");
+        }
+        else
+        {
+            const usrInfo = document.getElementById("usrInfoDescription");
+            usrInfo.innerHTML = `<p>Bienvenido: ${userName}</p>`;
+            document.getElementById("frmUserData").remove(); // Si el login es exitoso borro el  formulario del DOM
+            showServices(); //muestra los servicios disponibles
+        }
+   }
 }
-else
-{
-    alert("INICIO DE SESIÓN EXITOSO!");
-    const usrInfo = document.getElementById("usrInfoDescription");
-    usrInfo.innerHTML = `<p>Bienvenido: ${userName}</p>`;
+
+//Muestro los formularios a completar de acuerdo con el servicio elegido
+
+let btnServiceSoftware = document.getElementById("btnServiceSoftware");
+let btnServiceAudit = document.getElementById("btnServiceAudit");
+let btnServiceConsultant = document.getElementById("btnServiceConsultant");
+
+btnServiceSoftware.onclick = () =>{
+    showFormSoftware();
+}
+
+btnServiceAudit.onclick = () => {
+    hideFormSoftware();
+}
+
+btnServiceConsultant.onclick = () =>{
+    hideFormSoftware();
+}
+
+const btnSoftwareCalc = document.getElementById("btnSoftwareCalc");
+btnSoftwareCalc.onclick = () =>{
+    companyValidation();
+    baseAmountValidation();
+    usdPriceValidation();
     
 }
 
+const btnSoftwareClean = document.getElementById("btnSoftwareClean");
+btnSoftwareClean.onclick = () =>{
+    hideLabels();
+}
 
-//Valido el nombre de la empresa
- while (validaIngreso(nombreEmpresa, "string") !== "Perfecto!")
-{
-    nombreEmpresa = prompt("Ingresa el nombre de la empresa");
-    alert(validaIngreso(nombreEmpresa, "string"));
+
+
+// // Valido la cantidad de  establecimientos ingresada
+// cantEstablecimientos = prompt("Ingresa la cantidad de establecimientos de su empresa: "); 
+
+// while (isNaN(cantEstablecimientos) ||  cantEstablecimientos.trim()==="" || cantEstablecimientos === null || cantEstablecimientos<=0) 
+// {
+//     alert("La cantidad debe ser un número y mayor que 0");
+//     cantEstablecimientos = prompt("Ingresa la cantidad de establecimientos de su empresa: "); 
     
-}
+// }
 
-// valido el monto ingresado
-mtoImplementacionBaseUsd = prompt("Ingresa el monto base de implementación en USD: ");
+// cantEstablecimientos = cantEstablecimientos;
 
-while (isNaN(mtoImplementacionBaseUsd) ||  mtoImplementacionBaseUsd.trim()==="" || mtoImplementacionBaseUsd === null || mtoImplementacionBaseUsd<= 0) 
-{
-    alert("El Monto debe ser un número y mayor que 0");
-    mtoImplementacionBaseUsd = prompt("Ingresa el monto base de implementación en USD: ");
+// // Declaro la clase Establecimiento
+//  class Establecimiento
+//  {
+//     constructor (id, nombre, ciudad)
+//     {
+//         this.id = id;
+//         this.nombre = nombre.toUpperCase();
+//         this.ciudad = ciudad.toUpperCase();
+//     }
+//  }
+
+// const listaEstablecimiento = [];
+
+// // Por cada establecimiento ingresado solicito el nombre y la ciudad
+// for (let i = 0; i < cantEstablecimientos ; i++) {
+//         let idEstab = i;
+//         let nombreEstab = prompt("Ingrese el nombre del establecimiento N° "+ i);
+//         let ciudadEstab = prompt("Ingrese la ciudad del establecimiento N° "+i);
+//         listaEstablecimiento.push(new Establecimiento (idEstab, nombreEstab, ciudadEstab) );
+// }
+// alert("Perfecto!")
+
+// // Valido la cantidad de  normas ingresada
+// cantNormas = prompt("Ingresa la cantidad de normas certificadas: "); 
+
+// while (isNaN(cantNormas) ||  cantNormas.trim()==="" || cantNormas === null) 
+// {
+//     alert("La cantidad debe ser un número ");
+//     cantNormas = prompt("Ingresa la cantidad de normas certificadas: "); 
     
-}
-alert("Perfecto!")
-mtoImplementacionBaseUsd = mtoImplementacionBaseUsd;
+// }
+// alert("Perfecto!")
+// cantNormas = cantNormas;
 
-// Valido la cotización ingresada
+// // Valido la cantidad de  usuarios ingresada
+// cantUsuarios = prompt("Ingresa la cantidad de usuarios de su empresa: "); 
 
-cotizActualDolar = prompt("Ingresa la cotización del dólar actual - Blue ;)  "); 
-
-while (isNaN(cotizActualDolar) ||  cotizActualDolar.trim()==="" || cotizActualDolar === null || cotizActualDolar<=0 ) 
-{
-    alert("La cotización debe ser un número y mayor que 0");
-    cotizActualDolar = prompt("Ingresa la cotización del dólar actual - Blue ;)  "); 
-}
-alert("Perfecto!")
-cotizActualDolar = cotizActualDolar;
-
-// Valido la cantidad de  establecimientos ingresada
-cantEstablecimientos = prompt("Ingresa la cantidad de establecimientos de su empresa: "); 
-
-while (isNaN(cantEstablecimientos) ||  cantEstablecimientos.trim()==="" || cantEstablecimientos === null || cantEstablecimientos<=0) 
-{
-    alert("La cantidad debe ser un número y mayor que 0");
-    cantEstablecimientos = prompt("Ingresa la cantidad de establecimientos de su empresa: "); 
+// while (isNaN(cantUsuarios) ||  cantUsuarios.trim()==="" || cantUsuarios === null || cantUsuarios<=0) 
+// {
+//     alert("La cantidad debe ser un número y mayor que 0");
+//     cantUsuarios = prompt("Ingresa la cantidad de usuarios de su empresa: "); 
     
-}
+// }
+// alert("Perfecto!")
+// cantUsuarios = cantUsuarios;
 
-cantEstablecimientos = cantEstablecimientos;
+// // Valido la cantidad de  documentos  ingresada
+// cantDocumentos = prompt("Ingresa la cantidad de documentos de su empresa: "); 
 
-// Declaro la clase Establecimiento
- class Establecimiento
- {
-    constructor (id, nombre, ciudad)
-    {
-        this.id = id;
-        this.nombre = nombre.toUpperCase();
-        this.ciudad = ciudad.toUpperCase();
-    }
- }
-
-const listaEstablecimiento = [];
-
-// Por cada establecimiento ingresado solicito el nombre y la ciudad
-for (let i = 0; i < cantEstablecimientos ; i++) {
-        let idEstab = i;
-        let nombreEstab = prompt("Ingrese el nombre del establecimiento N° "+ i);
-        let ciudadEstab = prompt("Ingrese la ciudad del establecimiento N° "+i);
-        listaEstablecimiento.push(new Establecimiento (idEstab, nombreEstab, ciudadEstab) );
-}
-alert("Perfecto!")
-
-// Valido la cantidad de  normas ingresada
-cantNormas = prompt("Ingresa la cantidad de normas certificadas: "); 
-
-while (isNaN(cantNormas) ||  cantNormas.trim()==="" || cantNormas === null) 
-{
-    alert("La cantidad debe ser un número ");
-    cantNormas = prompt("Ingresa la cantidad de normas certificadas: "); 
+// while (isNaN(cantDocumentos) ||  cantDocumentos.trim()==="" || cantDocumentos === null || cantDocumentos<=0) 
+// {
+//     alert("La cantidad debe ser un número y mayor que 0");
+//     cantDocumentos = prompt("Ingresa la cantidad de documentos de su empresa: "); 
     
-}
-alert("Perfecto!")
-cantNormas = cantNormas;
+// }
+// alert("Perfecto!")
 
-// Valido la cantidad de  usuarios ingresada
-cantUsuarios = prompt("Ingresa la cantidad de usuarios de su empresa: "); 
+// cantDocumentos = cantDocumentos;
 
-while (isNaN(cantUsuarios) ||  cantUsuarios.trim()==="" || cantUsuarios === null || cantUsuarios<=0) 
-{
-    alert("La cantidad debe ser un número y mayor que 0");
-    cantUsuarios = prompt("Ingresa la cantidad de usuarios de su empresa: "); 
-    
-}
-alert("Perfecto!")
-cantUsuarios = cantUsuarios;
-
-// Valido la cantidad de  documentos  ingresada
-cantDocumentos = prompt("Ingresa la cantidad de documentos de su empresa: "); 
-
-while (isNaN(cantDocumentos) ||  cantDocumentos.trim()==="" || cantDocumentos === null || cantDocumentos<=0) 
-{
-    alert("La cantidad debe ser un número y mayor que 0");
-    cantDocumentos = prompt("Ingresa la cantidad de documentos de su empresa: "); 
-    
-}
-alert("Perfecto!")
-
-cantDocumentos = cantDocumentos;
-
- const quotationParameters = document.getElementById("quotationParameters");
- quotationParameters.innerHTML = `<h4> Datos para la cotización </h4>
-    <p>Nombre de la empresa: ${nombreEmpresa.toUpperCase()} </p>
-    <p>Monto base de implementación (en dólares): u$s ${mtoImplementacionBaseUsd} </p>
-    <p>Cotización del dólar blue: $ ${cotizActualDolar} </p>
-    <p>Cantidad de establecimientos:  ${cantEstablecimientos} </p>
-    `
-// Muestro los establecimientos ingresados
-for (const estab of listaEstablecimiento)
-{
-    const branchP = document.createElement("p");
-    branchP.innerText= `Establecimiento: ${estab.id}  - ${estab.nombre} - ${estab.ciudad}`
-    quotationParameters.appendChild(branchP);
-}
-const standardNumber = document.createElement("p");
-standardNumber.innerText = `Cantidad de normas certificadas: ${cantNormas}`
-quotationParameters.appendChild(standardNumber);
-const usersNumber = document.createElement("p");
-usersNumber.innerText = `Cantidad de usuarios:  ${cantUsuarios}`
-quotationParameters.appendChild(usersNumber);
-const documentNumber = document.createElement("p");
-documentNumber.innerText = `Cantidad de documentos:  ${cantDocumentos}`
-quotationParameters.appendChild(documentNumber);
+//  const quotationParameters = document.getElementById("quotationParameters");
+//  quotationParameters.innerHTML = `<h4> Datos para la cotización </h4>
+//     <p>Nombre de la empresa: ${nombreEmpresa.toUpperCase()} </p>
+//     <p>Monto base de implementación (en dólares): u$s ${mtoImplementacionBaseUsd} </p>
+//     <p>Cotización del dólar blue: $ ${cotizActualDolar} </p>
+//     <p>Cantidad de establecimientos:  ${cantEstablecimientos} </p>
+//     `
+// // Muestro los establecimientos ingresados
+// for (const estab of listaEstablecimiento)
+// {
+//     const branchP = document.createElement("p");
+//     branchP.innerText= `Establecimiento: ${estab.id}  - ${estab.nombre} - ${estab.ciudad}`
+//     quotationParameters.appendChild(branchP);
+// }
+// const standardNumber = document.createElement("p");
+// standardNumber.innerText = `Cantidad de normas certificadas: ${cantNormas}`
+// quotationParameters.appendChild(standardNumber);
+// const usersNumber = document.createElement("p");
+// usersNumber.innerText = `Cantidad de usuarios:  ${cantUsuarios}`
+// quotationParameters.appendChild(usersNumber);
+// const documentNumber = document.createElement("p");
+// documentNumber.innerText = `Cantidad de documentos:  ${cantDocumentos}`
+// quotationParameters.appendChild(documentNumber);
 
 
 function calculaImporte ()
@@ -275,9 +355,9 @@ function calculaImporte ()
     return valorFinal;
 }
 
-const price = document.getElementById("price");
- price.innerHTML = `<h4> Precio de la cotización</h4>
-    <p>De acuerdo con los parámetros ingresados, el valor es: $ ${calculaImporte()} </p>`
+// const price = document.getElementById("price");
+//  price.innerHTML = `<h4> Precio de la cotización</h4>
+//     <p>De acuerdo con los parámetros ingresados, el valor es: $ ${calculaImporte()} </p>`
 
 
 
