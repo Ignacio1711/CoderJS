2a1a006de521f79ebd8a83e440f7acc24a83069f
SGO Consultora <administracion@sgoconsultora.com>
2022-06-07 22:21:35 -0300

    Guarda usuarios en el local storage - Avances en registro de nuevos usuarios que se almacenan como oobjetos del LS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -3,231 +3,208 @@
 // Declaro e inicializo parámetros de cotización
 
 let nombreEmpresa = "";
-let mtoImplementacionBaseUsd=0;
-let cotizActualDolar=0;
-let cantEstablecimientos=0;
-const cantBaseEstablecimientos=1;
-let cantNormas=0;
-const cantBaseNorma=1;
-let cantUsuarios=0;
-const cantBaseUsuarios=60;
-let cantDocumentos=0;
-const cantBaseDocumentos=100;
-
-
+let mtoImplementacionBaseUsd = 0;
+let cotizActualDolar = 0;
+let cantEstablecimientos = 0;
+const cantBaseEstablecimientos = 1;
+let cantNormas = 0;
+const cantBaseNorma = 1;
+let cantUsuarios = 0;
+const cantBaseUsuarios = 60;
+let cantDocumentos = 0;
+const cantBaseDocumentos = 100;
 
 /**************  BEGIN VALIDATION FUNCTIONS *******************/
-
 
 //Validación de datos
 let message = "";
 
-function validaIngreso(valor, tipo)
-{    
-    if (valor === null ||   valor === "") 
-    {
+function validaIngreso(valor, tipo) {
+    if (valor === null || valor === "") {
         message = "El dato solicitado no puede estar vacío."
         return message;
-    }
-    else
-    {
-        if(typeof(valor) === tipo)
-            {
-                message="Perfecto!"
-                return message;
-            }
-        else
-            {
-                message = "Verifique el dato ingresado!";
-                return message;
-            }
+    } else {
+        if (typeof (valor) === tipo) {
+            message = "Perfecto!"
+            return message;
+        } else {
+            message = "Verifique el dato ingresado!";
+            return message;
+        }
     }
 }
 
 
-function userValidation(usrName, usrPassword)
-{
-     for (let i = 0; i < lstUsers.length; i++) {
-      if (lstUsers[i].usrName === usrName && lstUsers[i].usrPassword === usrPassword)
-      {
-          return true;
-      }            
+function userValidation(usrName, usrPassword) {
+    let listStoragedUsers = JSON.parse(localStorage.getItem("usuarios")); //parseo el JSON para buscar la coincidencia del usuario que se loguea
+
+    for (let i = 0; i < localStorage.length; i++) {
+        if (listStoragedUsers[i].usrName === usrName && listStoragedUsers[i].usrPassword === usrPassword) {
+            return true;
+        }
     }
 }
 
 function companyValidation() {
     const inputCompanyName = document.getElementById("inputCompanyName");
-    if (inputCompanyName.value.trim() === "")
-    {
+    if (inputCompanyName.value.trim() === "") {
         const lblCompanyName = document.getElementById("lblCompanyName");
         lblCompanyName.style.display = "block";
-        lblCompanyName.innerText="El nombre de la empresa no puede estar vacío.";
-        
-    }
-    else
-    {
-       return nombreEmpresa = inputCompanyName.value;
+        lblCompanyName.innerText = "El nombre de la empresa no puede estar vacío.";
+
+    } else {
+        return nombreEmpresa = inputCompanyName.value;
     }
 }
 
 function baseAmountValidation() {
     const inputBaseAmount = document.getElementById("inputBaseAmount");
-    if (inputBaseAmount.value.trim() === "" ||  isNaN(inputBaseAmount.value) ||   inputBaseAmount.value<= 0)
-    {
+    if (inputBaseAmount.value.trim() === "" || isNaN(inputBaseAmount.value) || inputBaseAmount.value <= 0) {
         const lblBaseAmount = document.getElementById("lblBaseAmount");
         lblBaseAmount.style.display = "block";
-        lblBaseAmount.innerText="El monto inicial debe ser mayor a 0 (cero)";
-    }
-    else
-    {
-        return mtoImplementacionBaseUsd =inputBaseAmount.value ;
+        lblBaseAmount.innerText = "El monto inicial debe ser mayor a 0 (cero)";
+    } else {
+        return mtoImplementacionBaseUsd = inputBaseAmount.value;
     }
 }
 
 function usdPriceValidation() {
     const inputUsDolarPrice = document.getElementById("inputUsDolarPrice");
-    if (inputUsDolarPrice.value.trim() === "" ||  isNaN(inputUsDolarPrice.value) ||   inputUsDolarPrice.value<= 0)
-    {
+    if (inputUsDolarPrice.value.trim() === "" || isNaN(inputUsDolarPrice.value) || inputUsDolarPrice.value <= 0) {
         const lblUsDolarPrice = document.getElementById("lblUsDolarPrice");
         lblUsDolarPrice.style.display = "block";
-        lblUsDolarPrice.innerText="La cotización del dólar debe ser mayor a 0 (cero)";
-    }
-    else
-    {
-        return cotizActualDolar =inputUsDolarPrice.value ;
+        lblUsDolarPrice.innerText = "La cotización del dólar debe ser mayor a 0 (cero)";
+    } else {
+        return cotizActualDolar = inputUsDolarPrice.value;
     }
 }
+
 function branchQuantityValidation() {
     const inputBranchsQuantity = document.getElementById("inputBranchsQuantity");
-    if (inputBranchsQuantity.value.trim() === "" ||  isNaN(inputBranchsQuantity.value) ||   inputBranchsQuantity.value<= 0)
-    {
+    if (inputBranchsQuantity.value.trim() === "" || isNaN(inputBranchsQuantity.value) || inputBranchsQuantity.value <= 0) {
         const lblBranchsQuantity = document.getElementById("lblBranchsQuantity");
         lblBranchsQuantity.style.display = "block";
-        lblBranchsQuantity.innerText="La cantidad  de establecimientos debe ser mayor a 0 (cero)";
-    }
-    else
-    {
-        return cantEstablecimientos =lblBranchsQuantity.value ;
+        lblBranchsQuantity.innerText = "La cantidad  de establecimientos debe ser mayor a 0 (cero)";
+    } else {
+        return cantEstablecimientos = lblBranchsQuantity.value;
     }
 }
+
 function standardQuantityValidation() {
     const inputStandardsQuantity = document.getElementById("inputStandardsQuantity");
-    if (inputStandardsQuantity.value.trim() === "" ||  isNaN(inputStandardsQuantity.value))
-    {
+    if (inputStandardsQuantity.value.trim() === "" || isNaN(inputStandardsQuantity.value)) {
         const lblStandardsQuantity = document.getElementById("lblStandardsQuantity");
         lblStandardsQuantity.style.display = "block";
-        lblStandardsQuantity.innerText="La cantidad  de normas debe ser válida";
-    }
-    else
-    {
-        return cantNormas =inputStandardsQuantity.value ;
+        lblStandardsQuantity.innerText = "La cantidad  de normas debe ser válida";
+    } else {
+        return cantNormas = inputStandardsQuantity.value;
     }
 }
+
 function usersQuantityValidation() {
     const inputUsersQuantity = document.getElementById("inputUsersQuantity");
-    if (inputUsersQuantity.value.trim() === "" ||  isNaN(inputUsersQuantity.value ||   inputUsersQuantity.value<= 0))
-    {
+    if (inputUsersQuantity.value.trim() === "" || isNaN(inputUsersQuantity.value || inputUsersQuantity.value <= 0)) {
         const lblUsersQuantity = document.getElementById("lblUsersQuantity");
         lblUsersQuantity.style.display = "block";
-        lblUsersQuantity.innerText="La cantidad  de usuarios debe ser mayor a cero (0).";
-    }
-    else
-    {
-        return cantUsuarios =inputUsersQuantity.value ;
+        lblUsersQuantity.innerText = "La cantidad  de usuarios debe ser mayor a cero (0).";
+    } else {
+        return cantUsuarios = inputUsersQuantity.value;
     }
 }
+
 function documentsQuantityValidation() {
     const inputDocumentsQuantity = document.getElementById("inputDocumentsQuantity");
-    if (inputDocumentsQuantity.value.trim() === "" ||  isNaN(inputDocumentsQuantity.value ||   inputDocumentsQuantity.value<= 0))
-    {
+    if (inputDocumentsQuantity.value.trim() === "" || isNaN(inputDocumentsQuantity.value || inputDocumentsQuantity.value <= 0)) {
         const lblDocumentsQuantity = document.getElementById("lblDocumentsQuantity");
         lblDocumentsQuantity.style.display = "block";
-        lblDocumentsQuantity.innerText="La cantidad  de documentos debe ser mayor a cero (0).";
-    }
-    else
-    {
-        return cantDocumentos =inputDocumentsQuantity.value ;
+        lblDocumentsQuantity.innerText = "La cantidad  de documentos debe ser mayor a cero (0).";
+    } else {
+        return cantDocumentos = inputDocumentsQuantity.value;
     }
 }
- 
- 
 
 /**************  END VALIDATION FUNCTIONS *******************/
 
 /*************** BEGIN FRONTEND FUNCTIONS **************** */
 
-function showServices(){
+function showServices() {
     const serviceContainer = document.getElementById("services");
-    serviceContainer.style.display='flex';
+    serviceContainer.style.display = 'flex';
 }
 
-function hideFormSoftware(){
+function hideFormSoftware() {
     const formSoftware = document.getElementById("frmSoftwareParameters");
     formSoftware.style.display = 'none'
 }
 
-function showFormSoftware(){
+function showFormSoftware() {
     const formSoftware = document.getElementById("frmSoftwareParameters");
-    formSoftware.style.display='flex';
+    formSoftware.style.display = 'flex';
     formSoftware.style.flexDirection = 'column';
 }
 
-function hideLabels (){
+function hideLabels() {
     const labels = document.getElementsByClassName("frmSoftwareParameters__lblError");
     for (let i = 0; i < labels.length; i++) {
-        labels[i].style.display='none';
-              
+        labels[i].style.display = 'none';
     }
 }
 
 /*************** END FRONTEND FUNCTIONS **************** */
 
-class Users
-{
-   constructor (usrName, usrPassword)
-   {
-       this.usrName = usrName;
-       this.usrPassword = usrPassword;
-   }
+class Users {
+    constructor(usrName, usrPassword) {
+        this.usrName = usrName;
+        this.usrPassword = usrPassword;
+    }
 }
 
 //  Hardcoded users list 
 
-const lstUsers = [
-    {usrName:"iprieto", usrPassword: "123456"},
-    {usrName:"mramos", usrPassword: "12345678"},
-    {usrName: "lvergara", usrPassword: "lvergara123" }
+const lstUsers = [{
+        usrName: "iprieto",
+        usrPassword: "123456"
+    },
+    {
+        usrName: "mramos",
+        usrPassword: "12345678"
+    },
+    {
+        usrName: "lvergara",
+        usrPassword: "lvergara123"
+    }
 ]
+
+//Guardo en el local storage los usuarios creados
+localStorage.setItem("usuarios", JSON.stringify(lstUsers));
 
 // Login
 
 const inputUser = document.getElementById("inputUser");
-const  inputPass = document.getElementById("inputPass");
+const inputPass = document.getElementById("inputPass");
 let btnLogin = document.getElementById("btnLogin");
 
-btnLogin.onclick = () =>{
-    
+btnLogin.onclick = () => {
+
     let userName = inputUser.value.trim();
     let userPassword = inputPass.value;
-    
-   if(userName ==="" ||  userPassword ==="" )
-   {
-       alert("El usuario y la contraseña son obligatorios");
-   }
-   else
-   {
-        if (!userValidation(userName, userPassword))
-        {
+
+    if (userName === "" || userPassword === "") {
+        alert("El usuario y la contraseña son obligatorios");
+    } 
+    else 
+    {
+        if (!userValidation(userName, userPassword)) {
             alert("Su usuario o contraseña no son válidos");
-        }
+        } 
         else
         {
             const usrInfo = document.getElementById("usrInfoDescription");
             usrInfo.innerHTML = `<p>Bienvenido: ${userName}</p>`;
-            document.getElementById("frmUserData").remove(); // Si el login es exitoso borro el  formulario del DOM
+            document.getElementById("usrLogin").remove(); // Si el login es exitoso borro el  formulario del DOM
             showServices(); //muestra los servicios disponibles
         }
-   }
+    }
 }
 
 //Muestro los formularios a completar de acuerdo con el servicio elegido
@@ -236,7 +213,7 @@ let btnServiceSoftware = document.getElementById("btnServiceSoftware");
 let btnServiceAudit = document.getElementById("btnServiceAudit");
 let btnServiceConsultant = document.getElementById("btnServiceConsultant");
 
-btnServiceSoftware.onclick = () =>{
+btnServiceSoftware.onclick = () => {
     showFormSoftware();
 }
 
@@ -244,12 +221,13 @@ btnServiceAudit.onclick = () => {
     hideFormSoftware();
 }
 
-btnServiceConsultant.onclick = () =>{
+btnServiceConsultant.onclick = () => {
     hideFormSoftware();
 }
 
+// Botón calcular cotización del servicio de software
 const btnSoftwareCalc = document.getElementById("btnSoftwareCalc");
-btnSoftwareCalc.onclick = () =>{
+btnSoftwareCalc.onclick = () => {
     companyValidation();
     baseAmountValidation();
     usdPriceValidation();
@@ -257,94 +235,75 @@ btnSoftwareCalc.onclick = () =>{
     standardQuantityValidation();
     usersQuantityValidation();
     documentsQuantityValidation();
-    
+    // pasó las validaciones de todos los campos - > realizo el cálculo
+    const price = document.getElementById("price");
+    price.innerHTML = `<h4> Precio de la cotización</h4>
+    <p>De acuerdo con los parámetros ingresados, el valor es: $ ${calculaImporte()} </p>`
+
 }
 
+// Limpia formulario de cotización del servicio de software
 const btnSoftwareClean = document.getElementById("btnSoftwareClean");
-btnSoftwareClean.onclick = () =>{
+btnSoftwareClean.onclick = () => {
     hideLabels();
+    nombreEmpresa = "";
+    mtoImplementacionBaseUsd = 0;
+    cotizActualDolar = 0;
+    cantEstablecimientos = 0;
+    cantNormas = 0;
+    cantUsuarios = 0;
+    cantDocumentos = 0;
+    const price = document.getElementById("price");
+    price.innerHTML = `<h4> Precio de la cotización</h4>
+    <p>De acuerdo con los parámetros ingresados, el valor es: $ 0.00</p>`
+
 }
 
 
-
-
-//  const quotationParameters = document.getElementById("quotationParameters");
-//  quotationParameters.innerHTML = `<h4> Datos para la cotización </h4>
-//     <p>Nombre de la empresa: ${nombreEmpresa.toUpperCase()} </p>
-//     <p>Monto base de implementación (en dólares): u$s ${mtoImplementacionBaseUsd} </p>
-//     <p>Cotización del dólar blue: $ ${cotizActualDolar} </p>
-//     <p>Cantidad de establecimientos:  ${cantEstablecimientos} </p>
-//     `
-// // Muestro los establecimientos ingresados
-// for (const estab of listaEstablecimiento)
-// {
-//     const branchP = document.createElement("p");
-//     branchP.innerText= `Establecimiento: ${estab.id}  - ${estab.nombre} - ${estab.ciudad}`
-//     quotationParameters.appendChild(branchP);
-// }
-// const standardNumber = document.createElement("p");
-// standardNumber.innerText = `Cantidad de normas certificadas: ${cantNormas}`
-// quotationParameters.appendChild(standardNumber);
-// const usersNumber = document.createElement("p");
-// usersNumber.innerText = `Cantidad de usuarios:  ${cantUsuarios}`
-// quotationParameters.appendChild(usersNumber);
-// const documentNumber = document.createElement("p");
-// documentNumber.innerText = `Cantidad de documentos:  ${cantDocumentos}`
-// quotationParameters.appendChild(documentNumber);
-
-
-function calculaImporte ()
-{
-    let valorParcial=0;
-    let valorEstab=0;
-    let valorNormas=0;
-    let valorUsuarios=0;
-    let valorDocs=0;
+function calculaImporte() {
+    let valorParcial = 0;
+    let valorEstab = 0;
+    let valorNormas = 0;
+    let valorUsuarios = 0;
+    let valorDocs = 0;
     valorParcial = mtoImplementacionBaseUsd * cotizActualDolar;
-   
+
     // Establecimientos
-    
+
     if (cantEstablecimientos > cantBaseEstablecimientos) // si es mayor a la cantidad base lo convierto en porcentaje (10% por cada uno de más) y se lo sumo al valor.
     {
         cantEstablecimientos = cantEstablecimientos - cantBaseEstablecimientos // le resto la base
-        
-        valorEstab  =  (valorParcial * (cantEstablecimientos * 0.1))
+
+        valorEstab = (valorParcial * (cantEstablecimientos * 0.1))
     }
 
     // Normas
     if (cantNormas > cantBaseNorma) // si es mayor a la cantidad base lo convierto en porcentaje (5% por cada norma de más) y se lo sumo al valor.
     {
         cantNormas = cantNormas - cantBaseNorma // le resto la base
-        
+
         valorNormas = (valorParcial * (cantNormas * 0.05))
     }
-    
+
     // Usuarios
     if (cantUsuarios > cantBaseUsuarios) // si es mayor a la cantidad base lo convierto en porcentaje (12% por cada 30 usuarios de más) y se lo sumo al valor.
     {
         cantUsuarios = cantUsuarios - cantBaseUsuarios;
-       
-        valorUsuarios = (valorParcial * (Math.trunc(cantUsuarios/30) * 0.12) ); // Divido los usuarios de más por 30, tomo su parte entera y la multiplico por  el porcentaje.
+
+        valorUsuarios = (valorParcial * (Math.trunc(cantUsuarios / 30) * 0.12)); // Divido los usuarios de más por 30, tomo su parte entera y la multiplico por  el porcentaje.
     }
-    
+
     // Documentos 
 
     if (cantDocumentos > cantBaseDocumentos) // si es mayor a la cantidad base lo convierto en porcentaje (5% por cada 30 documentos de más) y se lo sumo al valor.
     {
         cantDocumentos = cantDocumentos - cantBaseDocumentos;
 
-        
-        valorDocs = (valorParcial * (Math.trunc(cantDocumentos/30) * 0.05) ); // Divido los usuarios de más por 30, tomo su parte entera y la multiplico por  el porcentaje.
+
+        valorDocs = (valorParcial * (Math.trunc(cantDocumentos / 30) * 0.05)); // Divido los usuarios de más por 30, tomo su parte entera y la multiplico por  el porcentaje.
     }
 
     valorFinal = valorParcial + valorEstab + valorNormas + valorUsuarios + valorDocs;
 
     return valorFinal;
 }
-
-// const price = document.getElementById("price");
-//  price.innerHTML = `<h4> Precio de la cotización</h4>
-//     <p>De acuerdo con los parámetros ingresados, el valor es: $ ${calculaImporte()} </p>`
-
-
-
